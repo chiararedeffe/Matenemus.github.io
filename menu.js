440f48cee069cd785fab06f24458154c9e45c3e8
Chiararedeffe <84141676+chiararedeffe@users.noreply.github.com>
2023-03-20 19:36:32 +0100

    modifiche

--- menu.js
+++ menu.js
@@ -5,13 +5,14 @@ function cambia()
     console.log(aperto);
     if(!aperto)
     {
-            document.getElementById("main-menu").style.paddingBottom="468px";
-        
+            document.getElementById("main-menu").style.paddingBottom="395px";
+            document.getElementById("menu2").style.top="18%";
         aperto=true;
 
     }else
     {
         document.getElementById("main-menu").style.paddingBottom="0px";
+        document.getElementById("menu2").style.top="100%";
         aperto=false;
     }
 }
--- slider.js
+++ slider.js
@@ -1,4 +1,10 @@
 const paginazione = document.getElementById('paginazione');
+const labels = document.querySelectorAll('#paginazione > label');
+const pagine = document.getElementById('pagine');
+const radioBtn = document.querySelectorAll('#pagine > input[type=radio]');
+let paginaCorrente = 1;
+let toccoInizioX = 0;
+let toccoFineX = 0;
 
 paginazione.addEventListener('click', e => {
     //console.log(e);
@@ -9,11 +15,33 @@ paginazione.addEventListener('click', e => {
 });
 
 function setCheckedLabel(numeroPaginaSelezionata){
-    const labels = document.querySelectorAll('#paginazione > label');
-    for(let i=0; i<labels.length; i++) {
-        labels[i].classList.remove('lab-sel');
-        if(i+1 == numeroPaginaSelezionata) {
-            labels[i].classList.add('lab-sel');
-        }
+    labels[paginaCorrente-1].classList.remove('lab-sel');
+    labels[numeroPaginaSelezionata-1].classList.add('lab-sel');
+    paginaCorrente = numeroPaginaSelezionata;
+}
+
+pagine.addEventListener('touchstart', touchStart, false);
+pagine.addEventListener('touchsend', touchEnd, false);
+
+function touchStart(e) {
+    toccoInizioX = e.changedTouches[0].screenX;
+}
+
+function touchEnd(e) {
+    toccoFineX = e.changedTouches[0].screenX;
+    toccoFineX < toccoInizioX ? moveTo('next') : moveTo('prev');
+}
+
+function moveTo(direction) {
+    if(direction === 'next' && paginaCorrente < labels.lenght) {
+        labels[paginaCorrente -1].classList.remove('lab-sel');
+        labels[paginaCorrente].classList.add('lab-sel');
+        radioBtn[paginaCorrente].checked = true;
+        paginaCorrente++;
+    } else if(direction === 'prev' && paginaCorrente > 1) {
+        labels[paginaCorrente -1].classList.remove('lab-sel');
+        labels[paginaCorrente -2].classList.add('lab-sel');
+        radioBtn[paginaCorrente -2].checked = true;
+        paginaCorrente--;
     }
 }
